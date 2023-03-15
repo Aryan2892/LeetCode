8841ab9038f07118a6174c718ac8ffd1aca9a2d4
Aryan Bhasein <78354139+Aryan2892@users.noreply.github.com>
2023-03-15 19:29:37 +0530

    Linked List - Kunal Kushwaha
    Kunal Kushwaha code-along for leetcode questions

---
+++ LinkedList/main.java
@@ -0,0 +1,13 @@
+package KunalKushwaha;
+
+public class main {
+    public static void main(String[] args) {
+
+    LinkedList ll = new LinkedList();
+    ll.display();
+    ll.insertRec(900,0);
+    ll.display();
+    ll.insertRec(98,0);
+    ll.display();
+    }
+}
