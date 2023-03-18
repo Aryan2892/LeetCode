13499c7d8e460b707815c93415b56e95632db350
Aryan Bhasein <78354139+Aryan2892@users.noreply.github.com>
2023-03-18 12:29:49 +0530

    141. Linked List Cycle

---
+++ LinkedList/KunalKushwaha/CycleDetection.java
@@ -0,0 +1,44 @@
+package KunalKushwaha;
+
+public class CycleDetection {
+
+    class Node{
+        int val;
+        Node next;
+
+        public Node(int val){
+            this.val = val;
+        }
+
+        public Node(int val, Node next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private Node prev;
+    private int size;
+
+    public static boolean hasCycle(Node head){
+        Node fast = head;
+        Node slow = head;
+
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if(fast == slow){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+
+
+
+}
